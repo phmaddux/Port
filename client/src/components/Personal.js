726edc46d1f072af9f9644953a8b5bf74d934c7f
Hunter Maddux <phmaddux@gmail.com>
2017-11-29 08:16:11 -0500

    Fix

--- client/src/components/Personal.js
+++ client/src/components/Personal.js
@@ -20,10 +20,8 @@ class Personal extends Component {
                         <Image src="https://i.imgur.com/1TWG1bi.png" alt="" />
                     </div>
                     <h2 class="writing">Welcome to my resume! I'm Hunter Maddux, and as the header indicates, I am a man who wears many, though often very different, hats.</h2>
-                    <h3 class="writing">I have reccently reached the end of my time at General Asembly in their Web Development Immersive course, and begun the search for a long term occupation.</h3>
-                    <h3 class="writing">Interest in Ruby</h3>
-                    <h3 class="writing">As a person in the workplace I believe in always learning and striving for excellence, even when I have to learn on the job. Luckily I have proven I can both learn and continue to put out work in both my last job working on commerical real estate deals, and learning the basics needed for my programming career in three months.</h3>
-                    <h3 class="writing">Both with no previous experience I might add...</h3>
+                    <h3 class="writing">I have always enjoyed creating things and solving problems, which is why I chose to pursue web development as a career. </h3>
+                    <h3 class="writing">As a developer I hope to specialize in React and Ruby on rails as I help to build out new apps. As a person I believe in always learning and striving for excellence, even when I have to learn quickly on the job. Luckily I have proven I can both learn while continuing to put out quality work in both my last job working on commerical real estate deals, and learning the basics needed for my programming career in three months.</h3>
                     <h3 class="writing">I hope to find job that shares the same ideals as I do when it comes to the workplace. I am looking for a place that encourages the continuing the education of their employees, and understands the value of proper teamwork.</h3>
                 </Intro>
             </div>
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ Project.destroy_all
 whoHasIt = Project.new({
     name: "Who Has It?",
     photo: "https://i.imgur.com/E60w0Ag.png",
-    description: 'I drew inspiration for this site from the same place I always do with these projects my life. When I left Tennessee I realized a lot of my stuff was missing, and being the kind of person I am I relaized I had lent out a lot of my stuff, forgotten who I had given it to, and then had to way to track it down. Therefore I created "Who Has It?", through it I could log in as a user, see who I had loaned what to, whether or not the item had been returned. Each of the three models will have full CRUD functionality in the next update.',
+    description: 'I drew inspiration for this site from the same place I always do with these projects, my life. When I left Tennessee I realized a lot of my stuff was missing, and being the kind of person I am I relaized I had lent out a lot of my stuff, forgotten who I had given it to, and then had no way to track it down. Therefore I created "Who Has It?", through it I could log in as a user, see who I had loaned what to, whether or not the item had been returned. Each of the three models will have full CRUD functionality in the next update.',
     technical_description: 'This project is a MERN stack app built with Rails and React, containining three models including unique users, "RESTful routing", Postgres SQL for the database, and is deployed online via heroku.',
     link_to_project: "https://who-has-it.herokuapp.com/",
     link_to_readMe: "https://github.com/phmaddux/Who_has_it/blob/master/README.md",
@@ -14,7 +14,7 @@ whoHasIt.save
 projectVagabond = Project.new({
     name: "Project Vagabond",
     photo: "https://i.imgur.com/e9ltQKe.png",
-    description: "This project was to create a website that was about users leaving their thoughts on cities and attractions within the city. However it was also the largest group project we undertook together. It was my primary experience pair programming and honestly was my favorite project, seeing as I felt I learnt the most working in a group.",
+    description: "This project was to create a website that was about users leaving their thoughts on cities and attractions within the city. It was a group project we undertook together with 4 of us pair programming and planning all together. It was my primary experience pair programming and honestly was my favorite project as I felt I learnt the most working in a group.",
     technical_description: "This project was React blended with Ruby on Rails, we accomplished full CRUD functionality on the posts, used an interal API, and posted it to github.",
     link_to_project: "https://wander-wisely.herokuapp.com/",
     link_to_readMe: "https://github.com/ern19/project_vagabond/blob/master/README.md",
@@ -25,7 +25,7 @@ projectVagabond.save
 theForge = Project.new({
     name: "The Forge",
     photo: "https://i.imgur.com/rtJl5uz.png",
-    description: "This website was concieved from another passion of mine. Art, specifically metalworking ",
+    description: "This website was concieved from a passion of mine. Metalworking ",
     technical_description: 'This was the first "Full Stack" project that we did. I used the MERN stack, using react as the front end framework, and again focused on building "RESTful routes".',
     link_to_project: 'https://fathomless-basin-57658.herokuapp.com/',
     link_to_readMe: "https://github.com/phmaddux/project3",
